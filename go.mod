67fd7cc84651364422428a62a29aa73af17550e4
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2025-03-09 16:22:49 -0700

    adds some initial basic operations

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/adrianosela/wnlm
+
+go 1.23.6
+
+require (
+	github.com/go-ole/go-ole v1.3.0
+	golang.org/x/sys v0.31.0
+)
---
+++ i_network_list_manager.go
@@ -0,0 +1,65 @@
+//go:build windows
+
+package wnlm
+
+import (
+	"fmt"
+
+	"github.com/go-ole/go-ole"
+)
+
+// INetworkListManager represents the Windows INetworkListManager type as defined in
+// https://learn.microsoft.com/en-us/windows/win32/api/netlistmgr/nn-netlistmgr-inetworklistmanager.
+//
+// The Windows Global Unique Identifier (GUID) for this interface is DCB00000-570F-4A9B-8D69-199FDBA5723B.
+type INetworkListManager interface {
+	GetNetworkConnections() (IEnumNetworkConnections, error)
+	Release()
+}
+
+// iNetworkListManager is the default implementation of INetworkListManager.
+type iNetworkListManager struct {
+	dispatch *ole.Dispatch
+}
+
+// NewNetworkListManager initializes a new Windows NetworkListManager API client.
+func NewNetworkListManager() (INetworkListManager, error) {
+	// NOTE(@adrianosela): DCB00C01-570F-4A9B-8D69-199FDBA5723B is the
+	// well-known Windows Global ID for the NetworkListManager class ID.
+	if err := globalOleConn.Create("{DCB00C01-570F-4A9B-8D69-199FDBA5723B}"); err != nil {
+		return nil, fmt.Errorf("failed to create NetworkListManager object by program GUID: %v", err)
+	}
+	defer globalOleConn.Release()
+	dispatch, err := globalOleConn.Dispatch()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get dispatch object for NetworkListManager: %v", err)
+	}
+	return &iNetworkListManager{dispatch: dispatch}, nil
+}
+
+// GetNetworkConnections returns all network connections for the system by using the API call described in
+// https://learn.microsoft.com/en-us/windows/win32/api/netlistmgr/nf-netlistmgr-inetwork-getnetworkconnections.
+func (nlm *iNetworkListManager) GetNetworkConnections() (IEnumNetworkConnections, error) {
+	// NOTE(@adrianosela): GetNetworkConnections is the well-known operation name
+	// to retrieve network connections via the NetworkListManager program.
+	networkConnectionsVariant, err := nlm.dispatch.Call("GetNetworkConnections")
+	if err != nil {
+		return nil, fmt.Errorf("failed to call GetNetworkConnections on NetworkListManager object: %v", err)
+	}
+	idispatch := networkConnectionsVariant.ToIDispatch()
+	if idispatch == nil {
+		return nil, fmt.Errorf("failed to convert variant to IDispatch")
+	}
+	defer idispatch.Release()
+
+	networkConnections, err := NewNetworkConnections(idispatch)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get NetworkConnections from *ole.VARIANT: %v", err)
+	}
+	return networkConnections, nil
+}
+
+// Release releases the NetworkListManager object.
+func (nlm *iNetworkListManager) Release() {
+	nlm.dispatch.Release()
+}
